ae3ab7824a922e081d8d40c0c641c5a392125f05
Ashish Walke <ashishwalke1808@gmail.com>
2026-04-09 15:50:12 +0530

    Initial Commit

---
+++ program1.js
@@ -0,0 +1,37 @@
+let num = 34;
+function getNum(num) {
+    return "hello";
+    return num + 2;
+}
+function getUpper(val) {
+    return val.toUpperCase();
+}
+console.log(getNum(num));
+// getUpper(4);
+function signUpUser(name, email, isPaid) {
+}
+signUpUser("ashish", "ashish@dev", false);
+let loginUser = (name, email, isPaid = false) => {
+};
+loginUser("A", "A@A.com");
+// function getValue(myVal:number):boolean
+// {
+//     if(myVal>5)
+//     {
+//         return true
+//     }
+//     return "200 ok"
+// }
+
+const getHello = (s) => {
+    return "ashish";
+};
+console.log(getHello);
+
+// const heros=["thor","spiderman","ironman"]
+const heros=[1,2,3];
+
+heros.map(hero=>{
+    return `hero is ${hero}`
+})
+export {};
